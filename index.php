d25eccc6c93062a93940f7a61899f84dd164e0e1
Brian Simiyu <briansimiyuj@gmail.com>
2024-08-01 15:32:01 +0300

    Embed PHP code in HTML

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    
+    <h1>
 
-   echo "Hello World";
+        <?php
+        
+            echo "Hello World";
+        
+        ?>
 
-?>
+    </h1>
+
+</body>
+</html>
