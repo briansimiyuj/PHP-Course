0c2ffef9cc705877f2cfe69f5abe109e73a654b5
Brian Simiyu <briansimiyuj@gmail.com>
2024-08-06 10:58:44 +0300

    Basic number operations

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <?php
 
     $radius = 25;
-    $pi = 3.14
+    $pi = 3.14;
+
+    // echo $area = $pi * $radius ** 2;
+
+    // order of operations (BODMAS)
+
+    // echo 2 * (5 + 5) / 5;
+
+    // $radius++;
+    // echo $radius;
+
+    /* $radius--;
+    echo $radius; */
+
+    $age = 20;
+    // $age += 10; //same as $age = $age + 10; 
+    // $age -= 10; //same as $age = $age - 10;
+    // $age *= 2; //same as $age = $age * 2;
+    echo $age;
 
 ?>
 
