043111de11f36b49894a0826c2aad57c8198a3b5
Brian Simiyu <briansimiyuj@gmail.com>
2024-08-07 12:28:38 +0300

    Loose and strict comparisons

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
-    // echo 'Brian' < 'Simiyu'; // This will return true because 'B' comes before 'S' in the alphabets
-    // echo 'Water' > 'Fire'; // This will return true because 'W' comes after 'F' in the alphabets
-    // echo 'brian' > 'Brian'; // This will return true because an uppercase letter is considered less than a lowercase letter
-    // echo 'Brian' > 'Brian'; // This will return false because both Brian are the same
-    echo 'Brian' == 'brian'; // This will return false because an uppercase letter is considered less than a lowercase letter`
+    // echo 5 == '5'; // this is true because it is a loose comparison
+
+    // echo 5 === '5'; // this is false because it is a strict comparison
+
+    // echo true == '1'; // this is true because it is a loose comparison
+
+    // echo true === '1'; // this is false because it is a strict comparison
+
+    // echo false == ''; // this is true because it is a loose comparison
+
+    echo false === ''; // this is false because it is a strict comparison
 
 ?>
 
