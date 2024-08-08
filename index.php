8aa73273348a402f84e481df56d2b22b19dcea1e
Brian Simiyu <briansimiyuj@gmail.com>
2024-08-08 12:13:57 +0300

    Intro to functions

--- index.php
+++ index.php
@@ -1,35 +1,12 @@
 <?php
 
-    $products =[
+    function sayHello(){
 
-        ['name' => 'shiny star', 'price' => 20],
-        ['name' => 'green shell', 'price' => 10],
-        ['name' => 'red shell', 'price' => 15],
-        ['name' => 'gold coin', 'price' => 5],
-        ['name' => 'lightning bolt', 'price' => 40],
-        ['name' => 'banana skin', 'price' => 2]
-
-    ];
-
-    
-    foreach($products as $product){
-
-        if($product['name'] === 'lightning bolt'){
-            
-            break;
-
-        }
-
-
-        if($product['price'] > 15){
-
-            continue;
-
-        }
-
-        echo $product['name'] . '<br />';
+        echo 'Good morning Brian';
 
     }
+
+    sayHello();
 
 
 ?>
