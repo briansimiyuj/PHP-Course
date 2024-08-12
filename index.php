6c846d25a91d27816aa75426019ca94db6abfd19
Brian Simiyu <briansimiyuj@gmail.com>
2024-08-12 09:07:34 +0300

    Project setup

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Course</title>
-</head>
-<body>
 
-    <?php include("content.php"); ?>
-
-</body>
+    <?php include "templates/header.php"; ?>
+    
 </html>
