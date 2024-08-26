91247738e34fc890d39baafd9634aa3e415ba458
Brian Simiyu <briansimiyuj@gmail.com>
2024-08-26 15:17:49 +0300

    Connecting to a database

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+
+    // connect to database
+    $conn = mysqli_connect("localhost", "brian", "brian", "ninja_pizza");
+
+    // check connection
+
+    if(!$conn) echo 'Connection error: ' . mysqli_connect_error();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
