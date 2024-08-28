428171061b9479c9ed773fe1ac93cb148a964501
Brian Simiyu <briansimiyuj@gmail.com>
2024-08-28 10:47:02 +0300

    Ternary Operator

---
+++ sandbox.php
@@ -0,0 +1,21 @@
+<?php
+
+    $score = 50;
+
+    $score > 40 ? $message = "high score!" : $message = "low score :(";
+    
+    echo $message;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MySQL Course</title>
+</head>
+<body>
+    
+</body>
+</html>
