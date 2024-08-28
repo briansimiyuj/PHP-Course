2356b06ec3c1262b71ce8b7bc6ead2c9bef650f9
Brian Simiyu <briansimiyuj@gmail.com>
2024-08-28 08:40:09 +0300

    Getting the details of individual pizza

--- details.php
+++ details.php
@@ -1,4 +1,28 @@
-<?php?>
+<?php
+
+    include "config/db_connect.php";
+
+    // Check for GET request ID param
+
+    if(isset($_GET["id"])){
+
+        $id = mysqli_real_escape_string($conn, $_GET["id"]);
+
+        $sql = "SELECT * FROM pizzas WHERE ID = $id";
+
+        $result = mysqli_query($conn, $sql);
+
+        $pizza = mysqli_fetch_assoc($result);
+
+        mysqli_free_result($result);
+
+        mysqli_close($conn);    
+
+        print_r($pizza);
+
+    }
+
+?>
 
 <html lang="en">
 
