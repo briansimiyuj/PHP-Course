8d13b597998982b0940a86c16d382b0c07b4d041
Brian Simiyu <briansimiyuj@gmail.com>
2024-09-04 10:57:41 +0300

    Constructor function

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
         public $name;
         public $email;
 
+
+        public function __construct(){
+
+            $this->name = 'Mario';
+            $this->email = 'mario@thenetninja.co.uk';
+
+        }
+
         public function login(){
 
             echo "User logged in";
