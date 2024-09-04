179115c739ba10f1440594f65cfae41e4ce2e90d
Brian Simiyu <briansimiyuj@gmail.com>
2024-09-04 11:27:25 +0300

    Setter function

--- index.php
+++ index.php
@@ -25,12 +25,32 @@
             return $this->name;
 
         }
+        
+
+
+        public function setName($name){
+
+            if(is_string($name) && strlen($name) > 1){
+
+                $this->name = $name;
+
+                return "The name has been updated to $name";
+
+            }else{
+
+                return 'Not a valid name';
+
+            }
+
+        }
 
     }
 
 
     $userOne = new User('Brian', 'brian@thenetninja.co.uk');
 
-    echo $userOne->getName();
+    // echo $userOne->setName(50);
+
+    echo $userOne->setName('Justin');
     
 ?>
